cb64f72ce76370b227b9814c4b9600aa4304098f
Clownacy <Clownacy@users.noreply.github.com>
2022-09-02 12:30:49 +0100

    Move Switch-related code that was left behind.
    The call to this code was moved in
    078ac53de2cf3788f1b8c0242bb9b143a84a1025, but the code itself was
    not.

--- RSDKv5/RSDK/Core/RetroEngine.cpp
+++ RSDKv5/RSDK/Core/RetroEngine.cpp
@@ -1327,6 +1327,31 @@ void RSDK::ProcessDebugCommands()
 #endif
 }
 
+#ifdef __SWITCH__
+#include <switch.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/errno.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+static int32 s_nxlinkSock = -1;
+
+static void initNxLink()
+{
+    if (R_FAILED(socketInitializeDefault()))
+        return;
+
+    s_nxlinkSock = nxlinkStdio();
+    if (s_nxlinkSock >= 0)
+        printf("printf output now goes to nxlink server\n");
+    else
+        socketExit();
+}
+#endif
+
 void RSDK::InitCoreAPI()
 {
 #if RETRO_RENDERDEVICE_DIRECTX9 || RETRO_RENDERDEVICE_DIRECTX11
