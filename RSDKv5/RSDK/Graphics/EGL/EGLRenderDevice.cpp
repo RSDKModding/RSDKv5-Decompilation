f0f3cac5460f3076e5d0861e3f72f0e6a75c67c3
Chuli <rmgrichdeveloper@gmail.com>
2022-11-15 15:50:46 -0600

    egl nuts

--- RSDKv5/RSDK/Graphics/EGL/EGLRenderDevice.cpp
+++ RSDKv5/RSDK/Graphics/EGL/EGLRenderDevice.cpp
@@ -41,7 +41,6 @@ char _glFPrecision[30]; // len("precision mediump float;\n") -> 25
 
 const GLchar *backupVertex = R"aa(
 in_V vec3 in_pos;
-in_V vec4 in_color;
 in_V vec2 in_UV;
 out vec4 ex_color;
 out vec2 ex_UV;
@@ -49,7 +48,7 @@ out vec2 ex_UV;
 void main()
 {
     gl_Position = vec4(in_pos, 1.0);
-    ex_color    = in_color;
+    ex_color    = vec4(1.0);
     ex_UV       = in_UV;
 }
 )aa";
