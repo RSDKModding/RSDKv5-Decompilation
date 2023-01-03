94ebc51fca2d621f4da0e798cebd674d6f68d0dd
Mefiresu <15063879+Mefiresu@users.noreply.github.com>
2023-01-03 21:02:57 +0100

    Linux: Support for shader compilation
    Add OS check for glslc.

--- RSDKv5/Shaders/Vulkan/compile.py
+++ RSDKv5/Shaders/Vulkan/compile.py
@@ -1,17 +1,24 @@
-import os, subprocess
+import os, subprocess, platform
 
-PATH_TO_VK_SDK = os.environ['VK_SDK_PATH']
-GLSLC = PATH_TO_VK_SDK + "/bin/glslc.exe"
+if platform.system() == "Windows":
+    PATH_TO_VK_SDK = os.environ['VK_SDK_PATH']
+    GLSLC = PATH_TO_VK_SDK + "/bin/glslc.exe"
+elif platform.system() == "Linux":
+    GLSLC = "glslc"
+else:
+    print(f"Unsupported OS: {platform.system()}")
+    exit(1)
+
 
 subprocess.call([GLSLC, "",])
 
-try: 
+try:
     os.mkdir("../CSO-Vulkan")
 except: pass
 
 for p in os.scandir("."):
     if not p.name.endswith("py"):
-        try: 
+        try:
             subprocess.check_call([GLSLC, p.name, '-DRETRO_REV02=1', '-o', f"../CSO-Vulkan/{p.name}"])
         except subprocess.CalledProcessError as e:
             print("!!! error compiling", p.name)
