RSDKModding/RSDKv5-Decompilation

commit 481464a1d3c479cec159f422e1def4bc04dae373

BIG GUY
REFACTOR HOW STATIC LINKING WORKS:
link game to RSDK, set `RETRO_STANDALONE=0` for RSDK
add game to RSDK include path (uses new GameMain.h)

ADDED PORTAUDIO DEVICE
this should be favored over SDL

ADDED PROPER CMAKE COMPILATION
windows (DX9 and OGL) and linux (OGL) have been tested
windows favors vcpkg, linux favors pkgconfig
both will use sources for most if packages are missing
to compile with mania, use the new cmake in mania with -DWITH_RSDK=1 (defaulted to on, turn off with 0 or N or wtv)

(READMEs will be updated soon as cmake is now the preferred method of configuring)

NEW RETRO_DEV_EXTRA DEFINE
dev menu, empty spot up top

many many small bug fixes (especially to the function table)
many small edits done to help out some compilers

RSDKv5/RSDK/Audio/XAudio/XAudioDevice.hpp
@@ -15,12 +15,18 @@ class AudioDeviceCallback : public IXAudio2VoiceCallback
     void WINAPI OnVoiceError(void *pBufferContext, HRESULT Error) {}
 };
 
+// using clang to compile? stfu
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmicrosoft-exception-spec"
+
 class AudioEngineCallback : public IXAudio2EngineCallback
 {
-    void WINAPI OnProcessingPassStart() {}
-    void WINAPI OnProcessingPassEnd() {}
+    void WINAPI OnProcessingPassStart(void) {}
+    void WINAPI OnProcessingPassEnd(void) {}
     void WINAPI OnCriticalError(HRESULT Error);
 };
+
+#pragma GCC diagnostic pop
 
 struct AudioDevice : public AudioDeviceBase {
     static bool32 Init();

Android.mk
@@ -1,129 +1,0 @@
-LOCAL_PATH := $(call my-dir)
-RSDK_PATH := $(LOCAL_PATH)
-
-OGG_DIR := dependencies/android/libogg
-THEORA_DIR := dependencies/android/libtheora
-
-OGG_INCLUDES    := $(RSDK_PATH)/$(OGG_DIR)/include
-THEORA_INCLUDES := $(RSDK_PATH)/$(THEORA_DIR)/include \
-	                 $(RSDK_PATH)/$(THEORA_DIR)/lib
-
-######################################################################
-# OGG
-include $(CLEAR_VARS)
-
-LOCAL_ARM_MODE := arm
-LOCAL_MODULE   := libogg
-LOCAL_CFLAGS   := -ffast-math -fsigned-char -O2 -fPIC -DPIC \
-                  -DBYTE_ORDER=LITTLE_ENDIAN -D_ARM_ASSEM_ -w
-
-LOCAL_C_INCLUDES := $(OGG_INCLUDES)
-
-LOCAL_SRC_FILES := \
-    $(RSDK_PATH)/$(OGG_DIR)/src/bitwise.c \
-    $(RSDK_PATH)/$(OGG_DIR)/src/framing.c
-
-include $(BUILD_STATIC_LIBRARY)
-
-######################################################################
-# THEORA
-include $(CLEAR_VARS)
-
-LOCAL_ARM_MODE := arm
-LOCAL_MODULE   := libtheora
-LOCAL_CFLAGS   := -ffast-math -fsigned-char -O2 -fPIC -DPIC \
-                  -DBYTE_ORDER=LITTLE_ENDIAN -D_ARM_ASSEM_ -w
-
-LOCAL_C_INCLUDES := $(OGG_INCLUDES) $(THEORA_INCLUDES)
-
-LOCAL_SRC_FILES := \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/analyze.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/apiwrapper.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/bitpack.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/cpu.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/decapiwrapper.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/decinfo.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/decode.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/dequant.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/encapiwrapper.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/encfrag.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/encinfo.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/encode.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/encoder_disabled.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/enquant.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/fdct.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/fragment.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/huffdec.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/huffenc.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/idct.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/info.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/internal.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/mathops.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/mcenc.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/quant.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/rate.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/state.c \
-    $(RSDK_PATH)/$(THEORA_DIR)/lib/tokenize.c
-
-include $(BUILD_STATIC_LIBRARY)
-
-######################################################################
-#RSDK
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := RetroEngine
-
-LOCAL_CFLAGS := -fexceptions -frtti -DRSDK_USE_OGL
-
-LOCAL_C_INCLUDES := \
-	$(RSDK_PATH)/RSDKv5/ 					                \
-	$(RSDK_PATH)/dependencies/android/libogg/include/ 	    \
-	$(RSDK_PATH)/dependencies/android/libtheora/include/ 	\
-	$(RSDK_PATH)/dependencies/android/                  	\
-	$(RSDK_PATH)/dependencies/all/ 			            \
-	$(RSDK_PATH)/dependencies/all/tinyxml2/ 	            \
-	$(RSDK_PATH)/dependencies/all/iniparser/
-
-LOCAL_SRC_FILES := \
-	$(RSDK_PATH)/RSDKv5/main.cpp 							\
-	$(RSDK_PATH)/RSDKv5/RSDK/Core/RetroEngine.cpp  			\
-	$(RSDK_PATH)/RSDKv5/RSDK/Core/Math.cpp         			\
-	$(RSDK_PATH)/RSDKv5/RSDK/Core/Reader.cpp       			\
-	$(RSDK_PATH)/RSDKv5/RSDK/Core/Link.cpp        			\
-	$(RSDK_PATH)/RSDKv5/RSDK/Core/ModAPI.cpp       			\
-	$(RSDK_PATH)/RSDKv5/RSDK/Dev/Debug.cpp        			\
-	$(RSDK_PATH)/RSDKv5/RSDK/Storage/Storage.cpp       		\
-	$(RSDK_PATH)/RSDKv5/RSDK/Storage/Text.cpp         		\
-	$(RSDK_PATH)/RSDKv5/RSDK/Graphics/Drawing.cpp      		\
-	$(RSDK_PATH)/RSDKv5/RSDK/Graphics/Scene3D.cpp      		\
-	$(RSDK_PATH)/RSDKv5/RSDK/Graphics/Animation.cpp    		\
-	$(RSDK_PATH)/RSDKv5/RSDK/Graphics/Sprite.cpp       		\
-	$(RSDK_PATH)/RSDKv5/RSDK/Graphics/Palette.cpp      		\
-	$(RSDK_PATH)/RSDKv5/RSDK/Graphics/Video.cpp     			\
-	$(RSDK_PATH)/RSDKv5/RSDK/Audio/Audio.cpp        			\
-	$(RSDK_PATH)/RSDKv5/RSDK/Input/Input.cpp        			\
-	$(RSDK_PATH)/RSDKv5/RSDK/Scene/Scene.cpp        			\
-	$(RSDK_PATH)/RSDKv5/RSDK/Scene/Collision.cpp    			\
-	$(RSDK_PATH)/RSDKv5/RSDK/Scene/Object.cpp       			\
-	$(RSDK_PATH)/RSDKv5/RSDK/Scene/Objects/DefaultObject.cpp \
-	$(RSDK_PATH)/RSDKv5/RSDK/Scene/Objects/DevOutput.cpp     \
-	$(RSDK_PATH)/RSDKv5/RSDK/User/Core/UserAchievements.cpp  \
-	$(RSDK_PATH)/RSDKv5/RSDK/User/Core/UserCore.cpp     		\
-	$(RSDK_PATH)/RSDKv5/RSDK/User/Core/UserLeaderboards.cpp  \
-	$(RSDK_PATH)/RSDKv5/RSDK/User/Core/UserPresence.cpp     	\
-	$(RSDK_PATH)/RSDKv5/RSDK/User/Core/UserStats.cpp     	\
-	$(RSDK_PATH)/RSDKv5/RSDK/User/Core/UserStorage.cpp     	\
-	$(RSDK_PATH)/dependencies/all/tinyxml2/tinyxml2.cpp 		\
-	$(RSDK_PATH)/dependencies/all/iniparser/iniparser.cpp 	\
-	$(RSDK_PATH)/dependencies/all/iniparser/dictionary.cpp   \
-	$(RSDK_PATH)/dependencies/all/miniz/miniz.c              \
-	$(RSDK_PATH)/dependencies/android/androidHelpers.cpp
-
-
-LOCAL_SHARED_LIBRARIES := libogg libtheora
-LOCAL_LDLIBS := -lGLESv3 -lEGL -llog -lz -landroid -ljnigraphics -laaudio -lpaddleboat
-
-include $(BUILD_SHARED_LIBRARY)
-
-######################################################################

CMakeLists.txt
@@ -1,54 +1,19 @@
-# THIS CMAKE SHOULD BE USED FOR ANDROID ONLY
-# might make this all plats later but not needed for now, the Makefile works fine
-
 cmake_minimum_required(VERSION 3.7)
 
-set(OGG_DIR dependencies/android/libogg)
-set(THEORA_DIR dependencies/android/libtheora)
-add_library(
-    libogg
-    STATIC
-    ${OGG_DIR}/src/bitwise.c
-    ${OGG_DIR}/src/framing.c
-)
+project(RetroEngine)
 
-add_library(libtheora STATIC
-    ${THEORA_DIR}/lib/analyze.c
-    ${THEORA_DIR}/lib/apiwrapper.c
-    ${THEORA_DIR}/lib/bitpack.c
-    ${THEORA_DIR}/lib/cpu.c
-    ${THEORA_DIR}/lib/decapiwrapper.c
-    ${THEORA_DIR}/lib/decinfo.c
-    ${THEORA_DIR}/lib/decode.c
-    ${THEORA_DIR}/lib/dequant.c
-    ${THEORA_DIR}/lib/encapiwrapper.c
-    ${THEORA_DIR}/lib/encfrag.c
-    ${THEORA_DIR}/lib/encinfo.c
-    ${THEORA_DIR}/lib/encode.c
-    ${THEORA_DIR}/lib/encoder_disabled.c
-    ${THEORA_DIR}/lib/enquant.c
-    ${THEORA_DIR}/lib/fdct.c
-    ${THEORA_DIR}/lib/fragment.c
-    ${THEORA_DIR}/lib/huffdec.c
-    ${THEORA_DIR}/lib/huffenc.c
-    ${THEORA_DIR}/lib/idct.c
-    ${THEORA_DIR}/lib/info.c
-    ${THEORA_DIR}/lib/internal.c
-    ${THEORA_DIR}/lib/mathops.c
-    ${THEORA_DIR}/lib/mcenc.c
-    ${THEORA_DIR}/lib/quant.c
-    ${THEORA_DIR}/lib/rate.c
-    ${THEORA_DIR}/lib/state.c
-    ${THEORA_DIR}/lib/tokenize.c
-)
+set(RETRO_REVISION 3 CACHE STRING "What revision to compile for. Defaults to v5U = 3")
+option(RETRO_MOD_LOADER "Enables or disables the mod loader." ON)
 
-target_include_directories(libogg PRIVATE dependencies/android/libogg/include)
-target_include_directories(libtheora PRIVATE dependencies/android/libogg/include dependencies/android/libtheora/include)
+set(RETRO_NAME "RSDKv5")
 
-target_compile_options(libogg PRIVATE -ffast-math -fsigned-char -O2 -fPIC -DPIC -DBYTE_ORDER=LITTLE_ENDIAN -D_ARM_ASSEM_ -w)
-target_compile_options(libtheora PRIVATE -ffast-math -fsigned-char -O2 -fPIC -DPIC -DBYTE_ORDER=LITTLE_ENDIAN -D_ARM_ASSEM_ -w)
+if(RETRO_REVISION STREQUAL "3")
+    set(RETRO_NAME "RSDKv5U")
+endif()
 
-add_library(RetroEngine SHARED
+set(RETRO_OUTPUT_NAME ${RETRO_NAME} CACHE STRING "The exported name")
+
+set(RETRO_FILES
     RSDKv5/main.cpp
     RSDKv5/RSDK/Core/RetroEngine.cpp
     RSDKv5/RSDK/Core/Math.cpp
@@ -81,41 +46,108 @@ add_library(RetroEngine SHARED
     dependencies/all/iniparser/iniparser.cpp
     dependencies/all/iniparser/dictionary.cpp
     dependencies/all/miniz/miniz.c
-    dependencies/android/androidHelpers.cpp
 )
+
+if(NOT PLATFORM)
+    if(WIN32) # THIS ASSUMES VCPKG OR SOURCES !!!!!!!
+        set(PLATFORM "Windows" CACHE STRING "The platform to compile for.")
+    elseif(ANDROID)
+        set(PLATFORM "Android" CACHE STRING "The platform to compile for.")
+    elseif(SWITCH)
+        set(PLATFORM "Switch" CACHE STRING "The platform to compile for.")
+    else()
+        set(PLATFORM ${CMAKE_SYSTEM_NAME} CACHE STRING "The platform to compile for.")
+    endif()
+endif()
+
+include(platforms/${PLATFORM}.cmake)
+
+set_target_properties(RetroEngine PROPERTIES OUTPUT_NAME ${RETRO_OUTPUT_NAME})
+
+if(COMPILE_OGG)
+    set(OGG_DIR dependencies/${DEP_PATH}/libogg)
+    add_library(
+        libogg
+        GAME_STATIC
+        ${OGG_DIR}/src/bitwise.c
+        ${OGG_DIR}/src/framing.c
+    )
+
+    target_compile_options(libogg PRIVATE ${OGG_FLAGS})
+
+    target_include_directories(libogg PRIVATE ${OGG_DIR}/include)
+    target_include_directories(RetroEngine PRIVATE ${OGG_DIR}/include)
+    target_link_libraries(RetroEngine libogg)
+endif()
+
+if(COMPILE_THEORA)
+    set(THEORA_DIR dependencies/android/libtheora)
+
+    add_library(libtheora GAME_STATIC
+        ${THEORA_DIR}/lib/analyze.c
+        ${THEORA_DIR}/lib/apiwrapper.c
+        ${THEORA_DIR}/lib/bitpack.c
+        ${THEORA_DIR}/lib/cpu.c
+        ${THEORA_DIR}/lib/decapiwrapper.c
+        ${THEORA_DIR}/lib/decinfo.c
+        ${THEORA_DIR}/lib/decode.c
+        ${THEORA_DIR}/lib/dequant.c
+        ${THEORA_DIR}/lib/encapiwrapper.c
+        ${THEORA_DIR}/lib/encfrag.c
+        ${THEORA_DIR}/lib/encinfo.c
+        ${THEORA_DIR}/lib/encode.c
+        ${THEORA_DIR}/lib/encoder_disabled.c
+        ${THEORA_DIR}/lib/enquant.c
+        ${THEORA_DIR}/lib/fdct.c
+        ${THEORA_DIR}/lib/fragment.c
+        ${THEORA_DIR}/lib/huffdec.c
+        ${THEORA_DIR}/lib/huffenc.c
+        ${THEORA_DIR}/lib/idct.c
+        ${THEORA_DIR}/lib/info.c
+        ${THEORA_DIR}/lib/internal.c
+        ${THEORA_DIR}/lib/mathops.c
+        ${THEORA_DIR}/lib/mcenc.c
+        ${THEORA_DIR}/lib/quant.c
+        ${THEORA_DIR}/lib/rate.c
+        ${THEORA_DIR}/lib/state.c
+        ${THEORA_DIR}/lib/tokenize.c
+    )
+
+    target_compile_options(libtheora PRIVATE ${THEORA_FLAGS})
+
+    target_include_directories(libtheora PRIVATE ${THEORA_DIR}/include ${OGG_DIR}/include)
+    target_include_directories(RetroEngine PRIVATE ${THEORA_DIR}/include)
+    target_link_libraries(RetroEngine libtheora)
+endif()
 
 target_include_directories(RetroEngine PRIVATE
     RSDKv5/
-    dependencies/android/libogg/include/
-    dependencies/android/libtheora/include/
-    dependencies/android/
     dependencies/all/
     dependencies/all/tinyxml2/
     dependencies/all/iniparser/
 )
 
-target_compile_options(RetroEngine PRIVATE -fexceptions -frtti -DRSDK_USE_OGL -DRETRO_REVISION=3)
+if(DEFINED DEP_PATH)
+    target_include_directories(RetroEngine PRIVATE
+        dependencies/${DEP_PATH}/
+    )
+endif()
 
-find_package(games-controller REQUIRED CONFIG)
-find_package(game-activity REQUIRED CONFIG)
-find_package(games-frame-pacing REQUIRED CONFIG)
-find_package(oboe REQUIRED CONFIG)
+if(NOT DEFINED GAME_STATIC)
+    set(GAME_STATIC OFF)
+endif()
 
-set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -u GameActivity_onCreate")
-
-target_link_libraries(
-    RetroEngine
-    android
-    libogg
-    libtheora
-    EGL
-    GLESv3
-    log
-    z
-    jnigraphics
-    games-controller::paddleboat_static
-    game-activity::game-activity
-    games-frame-pacing::swappy_static
-    oboe::oboe
-    OpenSLES
+target_compile_definitions(RetroEngine PRIVATE
+    RETRO_REVISION=${RETRO_REVISION}
+    RSDK_USE_${RETRO_SUBSYSTEM}=1
+    RETRO_USE_MOD_LOADER=$<BOOL:${RETRO_MOD_LOADER}>
+    RETRO_STANDALONE=$<NOT:$<BOOL:${GAME_STATIC}>>
+    RETRO_DEV_EXTRA="${PLATFORM} - ${RETRO_SUBSYSTEM} - ${CMAKE_CXX_COMPILER_ID}"
 )
+
+if(NOT GAME_STATIC AND WITH_RSDK)
+    add_custom_command(TARGET RetroEngine POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+        $<TARGET_FILE:${GAME_NAME}>
+        $<TARGET_FILE_DIR:RetroEngine>)
+endif()

RSDKv5/RSDK/Audio/Audio.cpp
@@ -1,5 +1,7 @@
 #include "RSDK/Core/RetroEngine.hpp"
 
+using namespace RSDK;
+
 #if RETRO_REV0U
 #include "Legacy/AudioLegacy.cpp"
 #endif
@@ -12,15 +14,13 @@
 stb_vorbis *vorbisInfo = NULL;
 stb_vorbis_alloc vorbisAlloc;
 
-using namespace RSDK;
-
 SFXInfo RSDK::sfxList[SFX_COUNT];
 ChannelInfo RSDK::channels[CHANNEL_COUNT];
 
 char streamFilePath[0x40];
 uint8 *streamBuffer    = NULL;
 int32 streamBufferSize = 0;
-int32 streamStartPos   = 0;
+uint32 streamStartPos   = 0;
 int32 streamLoopPoint  = 0;
 
 #define LINEAR_INTERPOLATION_LOOKUP_DIVISOR 0x40 // Determines the 'resolution' of the lookup table.
@@ -32,6 +32,8 @@ float linearInterpolationLookup[LINEAR_INTERPOLATION_LOOKUP_LENGTH];
 #include "XAudio/XAudioDevice.cpp"
 #elif RETRO_AUDIODEVICE_SDL2
 #include "SDL2/SDL2AudioDevice.cpp"
+#elif RETRO_AUDIODEVICE_PORT
+#include "PortAudio/PortAudioDevice.cpp"
 #elif RETRO_AUDIODEVICE_OBOE
 #include "Oboe/OboeAudioDevice.cpp"
 #endif
@@ -97,7 +99,7 @@ void AudioDeviceBase::ProcessAudioMixing(void *stream, int32 length)
                             break;
                         }
                         else {
-                            channel->bufferPos -= channel->sampleLength;
+                            channel->bufferPos -= (uint32)channel->sampleLength;
                             channel->bufferPos += channel->loop;
 
                             sfxBuffer = &channel->samplePtr[channel->bufferPos];
@@ -135,7 +137,7 @@ void AudioDeviceBase::ProcessAudioMixing(void *stream, int32 length)
                     channel->bufferPos += next * 2;
 
                     if (channel->bufferPos >= channel->sampleLength) {
-                        channel->bufferPos -= channel->sampleLength;
+                        channel->bufferPos -= (uint32)channel->sampleLength;
 
                         streamBuffer = &channel->samplePtr[channel->bufferPos];
 
@@ -236,7 +238,7 @@ void RSDK::LoadStream(ChannelInfo *channel)
         channel->state = CHANNEL_IDLE;
 }
 
-int32 RSDK::PlayStream(const char *filename, uint32 slot, int32 startPos, uint32 loopPoint, bool32 loadASync)
+int32 RSDK::PlayStream(const char *filename, uint32 slot, uint32 startPos, uint32 loopPoint, bool32 loadASync)
 {
     if (!engine.streamsEnabled)
         return -1;

RSDKv5/RSDK/Audio/Audio.hpp
@@ -63,7 +63,7 @@ protected:
 
 void UpdateStreamBuffer(ChannelInfo *channel);
 void LoadStream(ChannelInfo *channel);
-int32 PlayStream(const char *filename, uint32 slot, int32 startPos, uint32 loopPoint, bool32 loadASync);
+int32 PlayStream(const char *filename, uint32 slot, uint32 startPos, uint32 loopPoint, bool32 loadASync);
 
 void LoadSfxToSlot(char *filename, uint8 slot, uint8 plays, uint8 scope);
 void LoadSfx(char *filePath, uint8 plays, uint8 scope);
@@ -72,6 +72,8 @@ void LoadSfx(char *filePath, uint8 plays, uint8 scope);
 
 #if RETRO_AUDIODEVICE_XAUDIO
 #include "XAudio/XAudioDevice.hpp"
+#elif RETRO_AUDIODEVICE_PORT
+#include "PortAudio/PortAudioDevice.hpp"
 #elif RETRO_AUDIODEVICE_SDL2
 #include "SDL2/SDL2AudioDevice.hpp"
 #elif RETRO_AUDIODEVICE_OBOE
@@ -94,7 +96,7 @@ inline uint16 GetSfx(const char *sfxName)
     return -1;
 }
 int32 PlaySfx(uint16 sfx, uint32 loopPoint, uint32 priority);
-inline void StopSfx(int32 sfx)
+inline void StopSfx(uint16 sfx)
 {
 #if !RETRO_USE_ORIGINAL_CODE
     LockAudioDevice();

RSDKv5/RSDK/Audio/Legacy/AudioLegacy.cpp
@@ -55,7 +55,7 @@ int32 RSDK::Legacy::PlayMusic(int32 trackID)
 void RSDK::Legacy::SetMusicVolume(int32 volume)
 {
     musicVolume = CLAMP(volume, 0, 100);
-    SetChannelAttributes(musicChannel, musicVolume * 0.01, 0.0, 1.0);
+    SetChannelAttributes(musicChannel, musicVolume * 0.01f, 0.f, 1.f);
 }
 
 void RSDK::Legacy::v4::SwapMusicTrack(const char *filePath, uint8 trackID, uint32 loopPoint, uint32 ratio)

RSDKv5/RSDK/Audio/PortAudio/PortAudioDevice.cpp
@@ -1,0 +1,43 @@
+uint8 AudioDevice::contextInitialized;
+PaStream *AudioDevice::stream;
+
+bool32 AudioDevice::Init()
+{
+    PaError result;
+    result = Pa_Initialize();
+    if (result != paNoError) {
+        PrintLog(PRINT_NORMAL, "[PA] Initialization failed: %s", Pa_GetErrorText(result));
+        return false;
+    }
+    result = Pa_OpenDefaultStream(&stream, 0, AUDIO_CHANNELS, paFloat32, AUDIO_FREQUENCY, MIX_BUFFER_SIZE, AudioCallback, nullptr);
+    if (result != paNoError) {
+        PrintLog(PRINT_NORMAL, "[PA] Opening stream failed: %s", Pa_GetErrorText(result));
+        return false;
+    }
+    result = Pa_StartStream(stream);
+    if (result != paNoError) {
+        PrintLog(PRINT_NORMAL, "[PA] Starting stream failed: %s", Pa_GetErrorText(result));
+        return false;
+    }
+    return true;
+}
+
+void AudioDevice::Release()
+{
+    Pa_StopStream(stream);
+    Pa_Terminate();
+}
+
+void AudioDevice::InitAudioChannels() { AudioDeviceBase::InitAudioChannels(); }
+
+int RSDK::AudioDevice::AudioCallback(const void *input, void *output, unsigned long frameCount, const PaStreamCallbackTimeInfo *timeInfo,
+                                      PaStreamCallbackFlags statusFlags, void *userData)
+{
+    (void)input;
+    (void)timeInfo;
+    (void)statusFlags;
+    (void)userData;
+
+    AudioDevice::ProcessAudioMixing(output, frameCount * AUDIO_CHANNELS);
+    return 0;
+}

RSDKv5/RSDK/Audio/PortAudio/PortAudioDevice.hpp
@@ -1,0 +1,36 @@
+#define LockAudioDevice()
+#define UnlockAudioDevice()
+
+#include <thread>
+
+namespace RSDK
+{
+class AudioDevice : public AudioDeviceBase
+{
+public:
+    static PaStream *stream;
+
+    static bool32 Init();
+    static void Release();
+
+    static void FrameInit() {}
+
+    inline static void HandleStreamLoad(ChannelInfo *channel, bool32 async)
+    {
+        if (async) {
+            std::thread thread(LoadStream, channel);
+            thread.detach();
+        }
+        else
+            LoadStream(channel);
+    }
+
+private:
+    static uint8 contextInitialized;
+
+    static void InitAudioChannels();
+
+    static int AudioCallback(const void *input, void *output, unsigned long frameCount, const PaStreamCallbackTimeInfo *timeInfo,
+                              PaStreamCallbackFlags statusFlags, void *userData);
+};
+} // namespace RSDK

commit 2512766436b709481bc8e40a8c59927703750646

Add preprocessor checks for GCC pragmas
This fixes the warnings and errors from building with the Visual Studio solution method introduced in 481464a.

RSDKv5/RSDK/Audio/XAudio/XAudioDevice.hpp
@@ -15,9 +15,11 @@ class AudioDeviceCallback : public IXAudio2VoiceCallback
     void WINAPI OnVoiceError(void *pBufferContext, HRESULT Error) {}
 };
 
+#ifdef __GNUC__
 // using clang to compile? stfu
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wmicrosoft-exception-spec"
+#endif
 
 class AudioEngineCallback : public IXAudio2EngineCallback
 {
@@ -26,7 +28,9 @@ class AudioEngineCallback : public IXAudio2EngineCallback
     void WINAPI OnCriticalError(HRESULT Error);
 };
 
+#ifdef __GNUC__
 #pragma GCC diagnostic pop
+#endif
 
 struct AudioDevice : public AudioDeviceBase {
     static bool32 Init();