9b75592ec84d0cf8f4db6623451416a71128adec
Mefiresu <15063879+Mefiresu@users.noreply.github.com>
2023-10-08 20:40:20 +0200

    LoadSpritesheet: Check for failed allocation
    On a failed allocation, the pixel data would be stored in a temporary buffer
    in image.Load() but surface->pixels would still be NULL, causing a hard crash.
    As a workaround, move the temporary allocation to LoadSpritesheet and assign
    it to surface->pixels.

--- RSDKv5/RSDK/Graphics/Sprite.cpp
+++ RSDKv5/RSDK/Graphics/Sprite.cpp
@@ -952,6 +952,16 @@ uint16 RSDK::LoadSpriteSheet(const char *filename, uint8 scope)
 
         surface->pixels = NULL;
         AllocateStorage((void **)&surface->pixels, surface->width * surface->height, DATASET_STG, false);
+#if !RETRO_USE_ORIGINAL_CODE
+        // Bug details: On a failed allocation, image.pixels will end up being reallocated in image.Load().
+        // Pixel data would then be loaded in this temporary buffer, but surface->pixels would never point to the actual data.
+        // This issue would only happen on cases where the STG mempool is full, such as ports with lower storage limits
+        // or with mods that use a lot of spritesheets.
+        // As a last resort, let's try a new allocation in TMP for surface->pixels.
+        // NOTE: This is a workaround, and will still cause a crash if the TMP allocation fails as well.
+        if (!surface->pixels)
+            AllocateStorage((void **)&surface->pixels, surface->width * surface->height, DATASET_TMP, false);
+#endif
         image.pixels = surface->pixels;
         image.Load(NULL, false);
 
