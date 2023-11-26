9013375594c6a08923b9e94543f0fc35ca4762d6
st×tic <rmgrichdeveloper@gmail.com>
2023-11-25 19:47:51 -0600

    :p

--- RSDKv5/RSDK/Core/Reader.cpp
+++ RSDKv5/RSDK/Core/Reader.cpp
@@ -279,7 +279,6 @@ bool32 RSDK::LoadFile(FileInfo *info, const char *filename, uint8 fileMode)
         }
     }
 #endif
-#endif
 
 #if RETRO_PLATFORM == RETRO_OSX || RETRO_PLATFORM == RETRO_ANDROID
     if (addPath) {
@@ -289,7 +288,8 @@ bool32 RSDK::LoadFile(FileInfo *info, const char *filename, uint8 fileMode)
     }
 #else
     (void)addPath;
-#endif
+#endif // ! RETRO_PLATFORM
+#endif // ! RETRO_MOD_LOADER
 
 #if !RETRO_USE_ORIGNAL_CODE
     // somewhat hacky that also pleases the mod gods
