40e19875963f5ffbb4cf5a3fb9b2bd6abea8ad52
Mefiresu <15063879+Mefiresu@users.noreply.github.com>
2024-07-16 22:55:41 +0200

    Avoid UB on gameTitle string concatenation
    sprintf invokes undefined behavior when used as both input and output.
    Replace with strcat, which matches the original code without UB.
    Fixes #273.

--- RSDKv5/RSDK/Core/RetroEngine.cpp
+++ RSDKv5/RSDK/Core/RetroEngine.cpp
@@ -672,7 +672,7 @@ void RSDK::InitEngine()
 
             Legacy::v4::LoadGameConfig("Data/Game/GameConfig.bin");
             if (!useDataPack)
-                sprintf_s(gameVerInfo.gameTitle, sizeof(gameVerInfo.gameTitle), "%s (Data Folder)", gameVerInfo.gameTitle);
+                strcat(gameVerInfo.gameTitle, " (Data Folder)");
             strcpy(gameVerInfo.version, "Legacy v4 Mode");
 
             RSDK::GenerateBlendLookupTable();
@@ -713,7 +713,7 @@ void RSDK::InitEngine()
 
             Legacy::v3::LoadGameConfig("Data/Game/GameConfig.bin");
             if (!useDataPack)
-                sprintf_s(gameVerInfo.gameTitle, sizeof(gameVerInfo.gameTitle), "%s (Data Folder)", gameVerInfo.gameTitle);
+                strcat(gameVerInfo.gameTitle, " (Data Folder)");
             strcpy(gameVerInfo.version, "Legacy v3 Mode");
 
             RSDK::GenerateBlendLookupTable();
@@ -1031,7 +1031,7 @@ void RSDK::LoadGameConfig()
 
         ReadString(&info, gameVerInfo.gameTitle);
         if (!useDataPack)
-            sprintf_s(gameVerInfo.gameTitle, sizeof(gameVerInfo.gameTitle), "%s (Data Folder)", gameVerInfo.gameTitle);
+            strcat(gameVerInfo.gameTitle, " (Data Folder)");
         ReadString(&info, gameVerInfo.gameSubtitle);
         ReadString(&info, gameVerInfo.version);
 
