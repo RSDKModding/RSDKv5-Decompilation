f4e879b14a6eb124f0937105782cb29114dead2a
MegAmi <angrybirdconnor@gmail.com>
2025-07-14 10:27:59 -0500

    Add suggested changes

--- RSDKv5/RSDK/Dev/devfont_to_hpp.py
+++ RSDKv5/RSDK/Dev/devfont_to_hpp.py
@@ -15,10 +15,7 @@ def encode_font(image_path):
     print(f"Opening {image_path}...")
 
     img = Image.open(image_path).convert("1")
-    iwidth, iheight = img.size
-    if iwidth != IMGWIDTH or iheight != IMGHEIGHT:
-        print(f"[ERROR] Image does not match expected size ({IMGWIDTH}x{IMGHEIGHT})")
-        return []
+    assert img.size == (IMGWIDTH, IMGHEIGHT), f"Image does not match expected size ({IMGWIDTH}x{IMGHEIGHT})"
 
     print("Encoding pixel data...")
     data = list()
@@ -40,35 +37,33 @@ def encode_font(image_path):
     return data
 
 def main():
-    if len(sys.argv) == 1:
-        print("[ERROR] No input file given")
-    else:
-        img_file = Path(sys.argv[1])
-        data = encode_font(img_file)
+    assert len(sys.argv) > 1, "No input file given"
 
-        if len(data) > 0:
-            # Using img_file.with_name() will save it in the same directory as img_file
-            output_file = img_file.with_name(img_file.stem + ".hpp")
-            print(f"Writing to {output_file}...")
-            try:
-                with output_file.open("w") as hpp_file:
-                    hpp_file.write(f"#ifndef DEVFONT_H\n#define DEVFONT_H\n\n")
-                    if CHARCOUNT == 128:
-                        hpp_file.write("// First 128 characters of Code page 437 on old IBM computers\n")
-                    hpp_file.write("uint8 devTextStencil[] = {\n    ")
+    img_file = Path(sys.argv[1])
+    data = encode_font(img_file)
 
-                    # Print the list while dividing it into lines of 24 values
-                    array = []
-                    for row in itertools.batched(data, 24):
-                        array.append(", ".join(pixel for pixel in row) + ",")
-                    hpp_file.write("\n    ".join(array)[:-1])
+    # Using img_file.with_name() will save it in the same directory as img_file
+    output_file = img_file.with_name(img_file.stem + ".hpp")
+    print(f"Writing to {output_file}...")
+    try:
+        with output_file.open("w") as hpp_file:
+            hpp_file.write(f"#ifndef DEVFONT_H\n#define DEVFONT_H\n\n")
+            if CHARCOUNT == 128:
+                hpp_file.write("// First 128 characters of Code page 437 on old IBM computers\n")
+            hpp_file.write("uint8 devTextStencil[] = {\n    ")
 
-                    hpp_file.write("\n};\n\n")
-                    hpp_file.write("#endif")
+            # Print the list while dividing it into lines of 24 values
+            array = []
+            for row in itertools.batched(data, 24):
+                array.append(", ".join(pixel for pixel in row) + ",")
+            hpp_file.write("\n    ".join(array)[:-1])
 
-                print("Done!")
-            except Exception as e:
-                print(f"[ERROR] {e}")
+            hpp_file.write("\n};\n\n")
+            hpp_file.write("#endif")
+
+        print("Done!")
+    except Exception as e:
+        print(f"[ERROR] {e}")
 
 if __name__ == "__main__":
     main()
